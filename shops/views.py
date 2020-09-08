35c3b9baa67799a274639083054eceb3964c9eaf
Yumina9 <saei5455@naver.com>
2020-09-08 21:20:09 +0900

    feat: Shop Detail Flower, Bouquet 조회 API 추가

--- shops/views.py
+++ shops/views.py
@@ -6,9 +6,15 @@ from rest_framework.response import Response
 from shops.models import Shop
 from shops.serializers import ShopSerializer
 from rest_framework.decorators import api_view, renderer_classes
+from flowers.serializers import FlowerSerializer
+from bouquets.serializers import BouquetSerializer
 
 @api_view(['GET'])
 def shop_list(request):
+    '''
+        전체 shop 리스트를 조회합니다.
+        URL: http://localhost:8000/shops
+    '''
  
     if request.method == 'GET':
         shops = Shop.objects.all()
@@ -25,6 +31,7 @@ def shop_list(request):
         return JsonResponse(serializer.errors, status=400)
 
 @csrf_exempt
+@api_view(['GET', 'PUT', 'DELETE'])
 def shop_detail(request, pk):
     try:
         shop = Shop.objects.get(pk=pk)
@@ -33,7 +40,9 @@ def shop_detail(request, pk):
 
     if request.method == 'GET':
         serializer = ShopSerializer(shop)
-        return JsonResponse(serializer.data)
+
+        # print(serializer.data)
+        return Response(serializer.data)
 
     elif request.method == 'PUT':
         data = JSONParser().parse(request)
@@ -46,3 +55,37 @@ def shop_detail(request, pk):
     elif request.method == 'DELETE':
         shop.delete()
         return HttpResponse(status=204)
+
+@api_view(['GET'])
+'''
+가게에 등록된 꽃 데이터 조회
+http://localhost:8000/shop/1/flowers
+'''
+def shop_detail_flowers(request, pk):
+    try:
+        shop = Shop.objects.get(pk=pk)
+    except Shop.DoesNotExist:
+        return HttpResponse(status=404)
+
+    if request.method == 'GET':
+        query = shop.flower_set.all()
+        serializer = FlowerSerializer(query, many=True)
+        return Response(serializer.data, status=200)
+
+
+@api_view(['GET'])
+'''
+가게에 등록된 꽃다발 데이터 조회
+http://localhost:8000/shop/1/bouquets
+'''
+def shop_detail_bouquets(request, pk):
+    try:
+        shop = Shop.objects.get(pk=pk)
+    except Shop.DoesNotExist:
+        return HttpResponse(status=400)
+
+    if request.method =='GET':
+        query = shop.bouquet_set.all()
+        serializer = BouquetSerializer(query, many=True)
+        return Response(serializer.data, status=200)
+        
