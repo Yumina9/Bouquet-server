7230167cd5d40743b666078d29acb899b3155b66
Yumina9 <saei5455@naver.com>
2020-09-20 22:48:17 +0900

    feat: add price model to the bouquet

--- bouquets/models.py
+++ bouquets/models.py
@@ -5,11 +5,13 @@ class Bouquet(models.Model):
     name = models.CharField(max_length=50)
     description = models.CharField(max_length=200)
     img = models.ImageField(upload_to='image')
+    price = models.IntegerField(null=True, blank=True)
     flower_name = models.CharField(max_length=100, null=True, blank=True)
     flower_color = models.CharField(max_length=10, null=True, blank=True)
     flower_count = models.IntegerField(null=True, blank=True)
     wrappingpaper_color = models.CharField(max_length=10, null=True, blank=True)
     ribbon_color = models.CharField(max_length=10, null=True, blank=True)
+    flower_price = models.IntegerField(null=True, blank=True)
     shops = models.ForeignKey(Shop, on_delete=models.CASCADE, null=True, blank=True)
 
 
