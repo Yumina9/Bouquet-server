1249e974ab2584a128dde8f51241fb4bf7222a43
Yumina9 <saei5455@naver.com>
2020-10-07 23:31:52 +0900

    bouquet price model 추가

--- bouquets/models.py
+++ bouquets/models.py
@@ -5,6 +5,7 @@ class Bouquet(models.Model):
     name = models.CharField(max_length=50)
     description = models.CharField(max_length=200)
     img = models.ImageField(upload_to='image')
+    bouquet_price = models.IntegerField(null=True, blank=True)
     price = models.IntegerField(null=True, blank=True)
     flower_name = models.CharField(max_length=100, null=True, blank=True)
     flower_color = models.CharField(max_length=10, null=True, blank=True)
