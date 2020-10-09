8871a2dd26910d56c32309717d951c54e719fd19
Yumina9 <saei5455@naver.com>
2020-10-09 23:51:38 +0900

    Modified Flower Model

--- flowers/models.py
+++ flowers/models.py
@@ -1,15 +1,16 @@
 from django.db import models
 from shops.models import Shop
 
+
 class Flower(models.Model):
-    name= models.CharField(max_length=100)
-    description =models.CharField(max_length=100)
+    name = models.CharField(max_length=100)
+    description = models.CharField(max_length=100)
     season = models.CharField(max_length=20)
     color = models.CharField(max_length=10)
     img = models.ImageField(upload_to="image")
     price = models.IntegerField(null=True, blank=True)
-    shops = models.ForeignKey(Shop, on_delete=models.CASCADE,  null=True, blank=True)
-
+    shops = models.ForeignKey(
+        Shop, on_delete=models.CASCADE,  null=True, blank=True)
 
     def __str__(self):
         return self.color + " "+self.name
