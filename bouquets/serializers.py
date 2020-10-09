9a138c7a0a0b91b80df47d361a38279b4fe0c539
Yumina9 <saei5455@naver.com>
2020-10-09 23:52:51 +0900

    Flower Serializer add Bouquet Serializer

--- bouquets/serializers.py
+++ bouquets/serializers.py
@@ -1,8 +1,11 @@
 from rest_framework import serializers
 from bouquets.models import Bouquet
-
+from flowers.serializers import FlowerSerializer
 
 class BouquetSerializer(serializers.ModelSerializer):
+
+    flower = FlowerSerializer(many=True, read_only=True)
     class Meta:
         model=Bouquet
         fields='__all__'
+
