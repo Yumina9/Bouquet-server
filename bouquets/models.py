ffae53e4088add671db93a017cf386e45618239a
Yumina9 <saei5455@naver.com>
2020-10-10 23:45:44 +0900

    ribbon, wrappingPaper connect ManyToMany to the bouquet

--- bouquets/models.py
+++ bouquets/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from flowers.models import Flower
+from ribbons.models import Ribbon
+from wrappingPapers.models import WrappingPaper
 from shops.models import Shop
 
 
@@ -10,10 +12,11 @@ class Bouquet(models.Model):
     bouquet_paper_price = models.IntegerField(null=True, blank=True)
     flower = models.ManyToManyField(
         Flower, null=True, blank=True)
+    ribbon = models.ManyToManyField(
+        Ribbon, null=True, blank=True)
+    wrappingpaper = models.ManyToManyField(
+        WrappingPaper, null=True, blank=True)
     flower_count = models.IntegerField(null=True, blank=True)
-    wrappingpaper_color = models.CharField(
-        max_length=10, null=True, blank=True)
-    ribbon_color = models.CharField(max_length=10, null=True, blank=True)
     shops = models.ForeignKey(
         Shop, on_delete=models.CASCADE, null=True, blank=True)
 
--- bouquets/serializers.py
+++ bouquets/serializers.py
@@ -1,10 +1,14 @@
 from rest_framework import serializers
 from bouquets.models import Bouquet
 from flowers.serializers import FlowerSerializer
+from ribbons.serializers import RibbonSerializer
+from wrappingPapers.serializers import WrappingPaperSerializer
 
 class BouquetSerializer(serializers.ModelSerializer):
 
     flower = FlowerSerializer(many=True, read_only=True)
+    ribbon = RibbonSerializer(many=True, read_only=True)
+    wrappingPaper = WrappingPaperSerializer(many=True, read_only=True)
     class Meta:
         model=Bouquet
         fields='__all__'
