84939ec3164b27eabaa551d7f29486439e7d3c19
Yumina9 <saei5455@naver.com>
2020-10-21 15:34:19 +0900

    주문 app 생성

---
+++ bouquet_order/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+from shops.models import Shop
+from users.models import User
+from bouquets.models import Bouquet
+from flowers.models import Flower
+
+class Bouquet_order(models.Model):
+
+    shops = models.ForeignKey(
+        Shop, on_delete=models.CASCADE, blank=True)
+
+    users = models.ForeignKey(
+        User, on_delete=models.CASCADE, blank=True)
+
+    bouquet = models.ForeignKey(Bouquet, on_delete=models.CASCADE, null=True)
+    flower = models.ForeignKey(Flower, on_delete=models.CASCADE, null=True)
+
+    flower_count = models.IntegerField(default=1)
+    ribbon = models.CharField(max_length=100, blank=True)
+    wrappingPaper = models.CharField(max_length=100, blank=True)
+    price = models.IntegerField(default=0)
+
+    # def __str__(self):
+    #     return self.shops.name +"의" +self.bouquet.name
---
+++ bouquet_order/views.py
@@ -0,0 +1,54 @@
+from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+
+# DRF(Django Rest Framework)에서 제공해주는 API 제너릭 뷰 참조
+from rest_framework import generics
+from .models import Bouquet_order
+from .serializers import BouquetOrderSerializer
+from django.views.decorators.csrf import csrf_exempt
+from shops.models import Shop
+from flowers.models import Flower
+from bouquets.models import Bouquet
+
+class ShopOrderList(APIView):
+    """
+    View to list all users in the system.
+
+    * Requires token authentication.
+    * Only admin users are able to access this view.
+    """
+    
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request, format=None):
+        order = Bouquet_order.objects.filter(shops__user=request.user)
+        serializer = BouquetOrderSerializer(order, many=True)
+        print("order", order)
+        return Response(data=serializer.data, status=200)
+
+
+class bouquet_order_list(generics.ListCreateAPIView):
+
+    # IsAuthenticated가 있어야만 request.user 사용가능
+    permission_classes = [IsAuthenticated]
+    queryset = Bouquet_order.objects.all()
+    serializer_class = BouquetOrderSerializer
+
+    
+    def perform_create(self, serializer):
+
+        # request.data에는 axios의 post요청에서 담아서 보낸 데이터들 키값으로 그대로 들어오게 되어있어
+        flower = Flower.objects.get(id=self.request.data['flower_id']) if self.request.data['flower_id'] else None
+        bouquet = Bouquet.objects.get(id=self.request.data['bouquet_id']) if self.request.data['bouquet_id'] else None
+        shop = Shop.objects.get(id=self.request.data['shop_id']) if self.request.data['shop_id'] else None
+        price = self.request.data['resultPrice'] if self.request.data['bouquet_id'] else 0
+
+        serializer.save(users=self.request.user, shops=shop, bouquet=bouquet, flower=flower, price=price)
+
+
+class bouquet_order_detail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Bouquet_order.objects.all()
+    serializer_class = BouquetOrderSerializer
+
